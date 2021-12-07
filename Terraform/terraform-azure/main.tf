7ff8bf04187ccbb8b795a0aaa33eb13592e32cb8
Stephan Marketsmüller <stephan.marketsmueller@pentlandfirth.com>
2021-12-07 18:20:48 +0100

    added basic azure terraform resource creation

---
+++ Terraform/terraform-azure/main.tf
@@ -0,0 +1,39 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  backend "remote" {
+    organization = "<TERRAFORM CLOUD ORG NAME GOES HERE"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    Environment = "PoC"
+    Team        = "DevOps"
+  }
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
---
+++ Terraform/terraform-azure/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type    = string
+  default = "myTFResourceGroup"
+}
+
+variable "location" {
+  type    = string
+  default = "westeurope"
+}
